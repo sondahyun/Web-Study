2b7e8513f0e2f6438b879886a9fbbf80a1af67f1
sondahyun <88223753+sondahyun@users.noreply.github.com>
2023-11-08 18:22:57 +0900

    dd

--- week3/umc-workbook/src/App.js
+++ week3/umc-workbook/src/App.js
@@ -1,6 +1,22 @@
+import { dummy } from './movieDummy';
+import Movie from './Components/Movie.jsx'
+
 function App() {
   return (
-    <div className="App">
+    <div>
+        <div className="app-container"> 
+        {
+           dummy.results.map((item) => {
+            return (
+              <Movie 
+                title={item.title}
+                poster_path={item.poster_path}
+                vote_average={item.vote_average}
+                />
+              )
+           })
+        }
+        </div>
     </div>
   );
 }
